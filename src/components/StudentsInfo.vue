45ef4cb95398188ac614e29160ba51fa2f8b63e5
Serhii <smokzzz1201@gmail.com>
2021-10-25 18:17:10 +0300

    day4

---
+++ src/components/StudentsInfo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+   
+     <center style="font-size:35px;"> <br> ПІБ: {{student.name}} 
+     <br> Оцінка: {{student.mark}} 
+     <br> Залік: <input type="checkbox"  v-bind:checked="student.isDonePr">
+     <br> Группа: {{student.group}}  </center> 
+     
+        <img  style = "display:flex;  font-size:25px; margin:auto ;" v-bind:src="student.photo">
+  
+    </div>
+
+</template>
+
+<script>
+import Vue from 'vue'
+
+
+export default {
+      props: {
+           id: '',
+       },
+       data: function() {
+           return {
+               student: {},
+           }},
+    
+mounted: function (){
+    Vue.axios.get("http://46.101.212.195:3000/students/"+this.id)
+    .then( (response) => {
+        console.log(response.data)
+        this.student = response.data;
+    })
+    
+}
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
 import Vue from 'vue'
-import App from './components/App.vue'
+import VueRouter from 'vue-router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+
+import App from './components/App.vue'
+import Students from './components/Students.vue'
+import StudentsInfo from './components/StudentsInfo.vue'
+
+const routes = [
+    { path: '/', component: Students},
+    { path: '/student-info/:id', component: StudentsInfo, props: true},
+]
+
+const router = new VueRouter({
+
+    routes
+})
+
     Vue.use(VueAxios, axios)
+    Vue.use(VueRouter)
+    
 
 new Vue({
    render: h => h(App),
    el: '#app',
+   router
 })
 
